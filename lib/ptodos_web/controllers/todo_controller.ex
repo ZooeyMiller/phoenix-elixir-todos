c1a4f25e15f5f75757ab5ed8ba8dfcb3d1bd0b8a
finnhodgkin <finnhodgkin@gmail.com>
2017-08-23 16:31:16 +0100

    adds create, delete and edit todo functionality #5 #6 #7 #8

--- lib/ptodos_web/controllers/todo_controller.ex
+++ lib/ptodos_web/controllers/todo_controller.ex
@@ -5,8 +5,9 @@ defmodule PtodosWeb.TodoController do
   alias Ptodos.Todos.Todo
 
   def index(conn, _params) do
+    changeset = Todos.change_todo(%Todo{})
     todos = Todos.list_todos()
-    render(conn, "index.html", todos: todos)
+    render(conn, "index.html", todos: todos, changeset: changeset)
   end
 
   def new(conn, _params) do
@@ -16,18 +17,12 @@ defmodule PtodosWeb.TodoController do
 
   def create(conn, %{"todo" => todo_params}) do
     case Todos.create_todo(todo_params) do
-      {:ok, todo} ->
+      {:ok, _todo} ->
         conn
-        |> put_flash(:info, "Todo created successfully.")
-        |> redirect(to: todo_path(conn, :show, todo))
+        |> redirect(to: todo_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "index.html", changeset: changeset)
     end
-  end
-
-  def show(conn, %{"id" => id}) do
-    todo = Todos.get_todo!(id)
-    render(conn, "show.html", todo: todo)
   end
 
   def edit(conn, %{"id" => id}) do
@@ -43,7 +38,7 @@ defmodule PtodosWeb.TodoController do
       {:ok, todo} ->
         conn
         |> put_flash(:info, "Todo updated successfully.")
-        |> redirect(to: todo_path(conn, :show, todo))
+        |> redirect(to: todo_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", todo: todo, changeset: changeset)
     end
@@ -54,7 +49,6 @@ defmodule PtodosWeb.TodoController do
     {:ok, _todo} = Todos.delete_todo(todo)
 
     conn
-    |> put_flash(:info, "Todo deleted successfully.")
     |> redirect(to: todo_path(conn, :index))
   end
 end
